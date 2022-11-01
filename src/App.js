39c9484553ddb9b93fa08a3fade14ef9c2729b79
JaznanOfficial <jaznanofficial@gmail.com>
2022-11-01 19:19:54 +0600

    practicing redux

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 import DynamicHooksCounter from "./Components/DynamicHooksCounter";
-import HooksCounter from "./Components/HooksCounter";
+
 
 function App() {
     return (
         <Provider store={store}>
             <div className="App">
-                <HooksCounter/>
+                
+                
                 <DynamicHooksCounter />
             </div>
         </Provider>
